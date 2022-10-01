1a8298283e401507dc2af3e557892bf78a058643
leewoooo <leewoooo.dev@gmail.com>
2022-10-01 22:04:23 +0900

    ✨Add: config setting (typeorm, config module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getConfigModule } from './config/config.module';
+import { getTypeormModule } from './config/typeorm/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [getConfigModule(), getTypeormModule()],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm/migration.config.ts
@@ -0,0 +1,26 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+dotenv.config({
+  path:
+    process.env.NODE_ENV === 'prod'
+      ? path.join(__dirname, '../env/.env.production')
+      : path.join(__dirname, '../env/.env.development'),
+});
+
+console.log(path.join(__dirname, '../../migrations/*.{ts,js}'));
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: process.env.NODE_ENV === 'prod' ? ['error'] : 'all',
+  namingStrategy: new SnakeNamingStrategy(),
+  migrations: [path.join(__dirname, '../../migrations/*.ts')],
+  migrationsTableName: 'migrations',
+});
