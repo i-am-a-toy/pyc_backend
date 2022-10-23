0a6f8b30bacaa8d22071024b50a07bca1037ee5f
leewoooo <leewoooo.dev@gmail.com>
2022-10-23 20:45:35 +0900

    💚Fix: build error (import error)

--- src/modules/user/v1/controllers/user-controller.ts
+++ src/modules/user/v1/controllers/user-controller.ts
@@ -1,12 +1,7 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { CreateUserRequest } from 'src/dto/user/requests/create-user.request';
+import { Controller, Inject } from '@nestjs/common';
 import { IUserService } from '../interfaces/user-service.interface';
 
 @Controller('users')
 export class UserController {
   constructor(@Inject('userService') private readonly userService: IUserService) {}
-  @Post()
-  join(@Body() req: CreateUserRequest) {
-    return this.userService.save(req);
-  }
 }
--- src/modules/user/v1/v1-user.module.ts
+++ src/modules/user/v1/v1-user.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserEntityModule } from 'src/entities/user/user-entity.module';
 import { UserController } from './controllers/user-controller';
-import { UserService } from './services/UserService';
 
 @Module({
   imports: [UserEntityModule],
   controllers: [UserController],
-  providers: [
-    {
-      provide: 'userService',
-      useClass: UserService,
-    },
-  ],
+  providers: [],
 })
 export class V1UserModule {}
