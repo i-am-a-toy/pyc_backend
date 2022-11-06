8134fa630026e85d71e709e78b4476d72cf61a9e
leewoooo <leewoooo.dev@gmail.com>
2022-11-06 17:41:35 +0900

    🔨Modifiy: add column naming

--- src/entities/base-time.entity.ts
+++ src/entities/base-time.entity.ts
@@ -4,9 +4,9 @@ export abstract class BaseTimeEntity {
   @PrimaryGeneratedColumn({ unsigned: true })
   id: number;
 
-  @CreateDateColumn({ nullable: false, type: 'timestamptz', comment: '데이터 생성 일자' })
+  @CreateDateColumn({ nullable: false, name: 'created_at', type: 'timestamptz', comment: '데이터 생성 일자' })
   createdAt: Date;
 
-  @UpdateDateColumn({ nullable: false, type: 'timestamptz', comment: '데이터 업데이트 일자' })
+  @UpdateDateColumn({ nullable: false, name: 'last_modified_at', type: 'timestamptz', comment: '데이터 업데이트 일자' })
   lastModifiedAt: Date;
 }
