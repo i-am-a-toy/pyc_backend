ee9f21e621397084ba6fab654869f2644611ff71
leewoooo <leewoooo@visionerp.org>
2022-11-17 17:14:39 +0900

    🔨Modifiy: listen port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(8000);
+  await app.listen(3000);
 }
 bootstrap();
