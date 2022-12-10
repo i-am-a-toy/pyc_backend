0a3689c058cffccd7da63178d28c9a1c6fad8b91
leewoooo <leewoooo.dev@gmail.com>
2022-12-10 12:14:18 +0900

    🏷️Update: add createdBy

--- src/dto/notice-comment/responses/notice-comment.response.ts
+++ src/dto/notice-comment/responses/notice-comment.response.ts
@@ -1,5 +1,4 @@
 import { CreatorDto } from 'src/dto/common/dto/creator.dto';
-import { LastModifierDto } from 'src/dto/common/dto/last-modifier.dto';
 import { NoticeComment } from 'src/entities/notice-comment/notice-comment.entity';
 
 export class NoticeCommentResponse {
@@ -9,16 +8,18 @@ export class NoticeCommentResponse {
   readonly comment: string;
   readonly creator: CreatorDto;
   readonly createdAt: Date;
+  readonly createdBy: number;
   readonly lastModifiedAt: Date;
 
   constructor(e: NoticeComment) {
-    const { id, churchId, noticeId, comment, creator, cUser } = e;
+    const { id, churchId, noticeId, comment, creator, cUser, createdBy, createdAt } = e;
     this.id = id;
     this.churchId = churchId;
     this.noticeId = noticeId;
     this.comment = comment;
     this.creator = new CreatorDto(cUser, creator);
-    this.createdAt = e.createdAt;
+    this.createdAt = createdAt;
+    this.createdBy = createdBy;
     this.lastModifiedAt = e.lastModifiedAt;
   }
 }
