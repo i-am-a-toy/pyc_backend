cdb0625d4cd1dcf8bd9d5b0dd9087cf6008c5f13
leewoooo <leewoooo.dev@gmail.com>
2023-01-02 22:00:05 +0900

    ⚡️Improve: apply @Injectable & remove async

--- src/core/database/typeorm/transaction.middleware.ts
+++ src/core/database/typeorm/transaction.middleware.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { NestMiddleware } from '@nestjs/common/interfaces';
 import { createNamespace, getNamespace } from 'cls-hooked';
 import { NextFunction, Request, Response } from 'express';
@@ -7,23 +7,24 @@ import { EntityManager } from 'typeorm';
 export const PYC_NAMESPACE = 'namespace/pyc';
 export const PYC_ENTITY_MANAGER = 'namespace/entity-manager';
 
+@Injectable()
 export class TransactionMiddleware implements NestMiddleware {
   private readonly logger: Logger = new Logger(TransactionMiddleware.name);
   constructor(private readonly em: EntityManager) {}
 
-  use(_req: Request, _res: Response, next: NextFunction) {
+  use(_req: Request, _res: Response, next: NextFunction): void {
     const namespace = getNamespace(PYC_NAMESPACE) ?? createNamespace(PYC_NAMESPACE);
     this.logger.log(`Hit TransactionMiddleware`);
-    return namespace.runPromise(async () => {
+    namespace.run(async () => {
       this.logger.log(`PYC_NAMESPACE Run with status: ${!!namespace.active}`);
       Promise.resolve()
-        .then(() => this.setEntityManager(this.em))
+        .then(() => this.setEntityManager())
         .then(next);
     });
   }
 
-  private setEntityManager(em: EntityManager) {
+  private setEntityManager() {
     const namespace = getNamespace(PYC_NAMESPACE)!;
-    namespace.set<EntityManager>(PYC_ENTITY_MANAGER, em);
+    namespace.set<EntityManager>(PYC_ENTITY_MANAGER, this.em);
   }
 }
