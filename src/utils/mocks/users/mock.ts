9b7cef5185a19c956899c8a6f0e2560af077cde1
leewoooo <leewoooo.dev@gmail.com>
2023-01-09 00:51:12 +0900

    ⚡️Improve: add getMockUsers

--- src/utils/mocks/users/mock.ts
+++ src/utils/mocks/users/mock.ts
@@ -1,11 +1,82 @@
 import { plainToInstance } from 'class-transformer';
 import { CreateUserRequest } from 'src/dto/user/requests/create-user.request';
 import { Cell } from 'src/entities/cell/cell.entity';
+import { Church } from 'src/entities/church/church.entity';
 import { Address } from 'src/entities/embedded/address.entity';
 import { User } from 'src/entities/user/user.entity';
 import { Gender } from 'src/types/gender/gender.type';
 import { Rank } from 'src/types/rank/rank.type';
 import { Role } from 'src/types/role/role.type';
+
+export const getMockUsers = (church: Church, cell?: Cell): User[] => {
+  return [
+    User.of(
+      church,
+      cell ?? null,
+      'userA',
+      1,
+      Role.LEADER,
+      Rank.NONE,
+      Gender.NONE,
+      '1996-11-27',
+      new Address('12345', '서울'),
+      '010-1234-5678',
+      false,
+    ),
+    User.of(
+      church,
+      cell ?? null,
+      'userB',
+      1,
+      Role.LEADER,
+      Rank.NONE,
+      Gender.NONE,
+      '1996-11-27',
+      new Address('12345', '서울'),
+      '010-1234-5678',
+      false,
+    ),
+    User.of(
+      church,
+      cell ?? null,
+      'userC',
+      1,
+      Role.MEMBER,
+      Rank.NONE,
+      Gender.NONE,
+      '1996-11-27',
+      new Address('12345', '서울'),
+      '010-1234-5678',
+      false,
+    ),
+    User.of(
+      church,
+      cell ?? null,
+      'userD',
+      1,
+      Role.MEMBER,
+      Rank.NONE,
+      Gender.NONE,
+      '1996-11-27',
+      new Address('12345', '서울'),
+      '010-1234-5678',
+      false,
+    ),
+    User.of(
+      church,
+      cell ?? null,
+      'userE',
+      1,
+      Role.NEWBIE,
+      Rank.NONE,
+      Gender.NONE,
+      '1996-11-27',
+      new Address('12345', '서울'),
+      '010-1234-5678',
+      false,
+    ),
+  ];
+};
 
 export const mockCreateNewbieRequest: CreateUserRequest = plainToInstance(CreateUserRequest, {
   name: 'userA',
